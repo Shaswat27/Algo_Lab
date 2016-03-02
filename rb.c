f0f57d84e8740f40de32a9a2a371b560668d904e
Shaswat27 <shaswatdatta@hotmail.com>
2016-03-02 07:37:05 +0530

    Task Scheduling corrected

--- rb.c
+++ rb.c
@@ -53,7 +53,7 @@ void right_rotate(process** root, process* n)
 
 	if(n->parent == NULL)
 		*root = y;
-	else if(n = n->parent->right)
+	else if(n == n->parent->right)
 		n->parent->right = y;
 	else
 		n->parent->left = y;
@@ -129,8 +129,6 @@ void insert(process** root, process* node)
 {
 	bst_insert(root, node);
 
-	//printf("%d\n", node->parent->right->executionTime);
-
 	insert1(root, node);
 }
 
@@ -159,7 +157,6 @@ process* successor(process* n)
 
 int is_black(process* node)
 {
-	//printf("Clause\n");
 	if(node!=NULL)
 	{
 		if(node->color==0)
@@ -173,7 +170,6 @@ int is_black(process* node)
 
 void fix_delete(process** root, process* node, process* nodeParent, int nodeIsLeft) 
 {
-    //printf("node: %d\n", (*root)->executionTime);
     while (node != (*root) && is_black(node)) 
     {
         process* w;
@@ -182,9 +178,7 @@ void fix_delete(process** root, process* node, process* nodeParent, int nodeIsLe
         {
         	w = nodeParent->right;
 
-        	//printf("Clause\n");
-
-            if (is_black(w) != 1) 
+        	if (is_black(w) != 1) 
             {
             	w->color = 0;
                 nodeParent->color = 1;
@@ -193,37 +187,47 @@ void fix_delete(process** root, process* node, process* nodeParent, int nodeIsLe
                 w = nodeParent->right;
             }
 
-            if (is_black(w->left)==1 && is_black(w->right)==1) 
+            if(w!=NULL)
             {
-                w->color = 1;
-                node = nodeParent;
+            	if (is_black(w->left)==1 && is_black(w->right)==1) 
+            	{
+                	w->color = 1;
+                	node = nodeParent;
+                	nodeParent = node->parent;
+                	if(nodeParent!=NULL)
+                		nodeIsLeft = (node == nodeParent->left)?1:0;
+                	else
+                		nodeIsLeft = 0;
+            	} 
+            	else 
+            	{
+                	if (is_black(w->right) == 1) 
+                	{
+                   		w->left->color = 0;
+                   		w->color = 1;
+                    	right_rotate(root, w);
+                    	w = nodeParent->right;
+                	}
+
+                	w->color = nodeParent->color;
+                	nodeParent->color = 0;
+                	if (w->right != NULL) 
+                	{
+                   		w->right->color = 0;
+                	}
+                	left_rotate(root, nodeParent);
+                	node = *root;
+                	nodeParent = NULL;
+            	}
+            }
+            else  //if w is NULL
+            {
+            	node = nodeParent;
                 nodeParent = node->parent;
-                //printf("Clause2\n");
                 if(nodeParent!=NULL)
                 	nodeIsLeft = (node == nodeParent->left)?1:0;
                 else
                 	nodeIsLeft = 0;
-            } 
-            else 
-            {
-                
-                if (is_black(w->right) == 1) 
-                {
-                    w->left->color = 0;
-                    w->color = 1;
-                    right_rotate(root, w);
-                    w = nodeParent->right;
-                }
-
-                w->color = nodeParent->color;
-                nodeParent->color = 0;
-                if (w->right != NULL) 
-                {
-                    w->right->color = 0;
-                }
-                left_rotate(root, nodeParent);
-                node = *root;
-                nodeParent = NULL;
             }
         } 
         else 
@@ -270,9 +274,6 @@ void fix_delete(process** root, process* node, process* nodeParent, int nodeIsLe
             }
         }
     }
-
-    //printf("\n\nChange Node Color: %d\n\n", node->executionTime);
-
     node->color = 0;
 }
 
@@ -390,34 +391,16 @@ void scheduler(int N, int x, int M, int t, int c, process** root)
 			printf("node insert: %d\n", new_process->executionTime);
 			insert(root, new_process);
 
-			//if(c==3)
-        	//	printf("root = %d, root->left = %d, root->right = %d\n", (*root)->color, (*root)->left->color,(*root)->right->color);
-
 			x++;
 		}	
-		//if(c==5)
-		//printf("\nRoot: %d, %d, %d, %d\n\n", (*root)->color, (*root)->left->color, (*root)->right->color, (*root)->left->right->color);
 	}
 
 	if(x>0)
 	{
 		
 		process* execute = min(*root);
-		
-		//printf("before delete\n");
-
-		/*if(c==1)
-			printf("root = %d, color = %d\n", (*root)->executionTime, (*root)->color);
-
-		if(c==2)
-			printf("root = %d, color = %d\n, root->right = %d\n, color = %d", (*root)->executionTime, (*root)->color, (*root)->right->executionTime, (*root)->right->color);
-		*/
-		//if(*root != NULL && (*root)->left!=NULL && (*root)->right!=NULL)
-		//printf("\n\nroot = %d, root->left = %d, root->right = %d\n", (*root)->executionTime, (*root)->left->executionTime,(*root)->right->executionTime);
 
 		execute = delete(root, execute);
-
-		//printf("after delete\n");
 
 		execute->executionTime -= t*(execute->priority);
 
@@ -437,72 +420,17 @@ void scheduler(int N, int x, int M, int t, int c, process** root)
 
 void main()
 {
-	int N = 10, x = 0, M = 10; //max number of live processes, current number of processes, number of processes to be completed
+	int N = 1000, x = 0, M = 1000; //max number of live processes, current number of processes, number of processes to be completed
 	int t = 50; //time for each scheduling = priority*t;
 	int c = 0; //total number of processes created
 
-	//srand(time(NULL));
+	srand(time(NULL));
 
 	process* processTree = NULL;
 
 	scheduler(N, x, M, t, c, &processTree);
 
-	/*process* n1 = (process *)malloc(sizeof(process));
-	n1->processId = 1;
-	n1->executionTime = 10;
-	n1->color = 0;
-	n1->parent = NULL;
-	n1->left = NULL;
-	n1->right = NULL;
-
-	process* processTree = NULL; //root of the process tree
-
-	insert(&processTree, n1);
-
-	process* n2 = (process *)malloc(sizeof(process));
-	n2->processId = 1;
-	n2->executionTime = 9;
-	n2->color = 1;
-	n2->parent = NULL;
-	n2->left = NULL;
-	n2->right = NULL;
-
-	insert(&processTree, n2);
-
-	//printf("%d\n%d\n", n1->executionTime, n1->->executionTime);
-
-	process* n3 = (process *)malloc(sizeof(process));
-	n3->processId = 1;
-	n3->executionTime = 8;
-	n3->color = 1;
-	n3->parent = NULL;
-	n3->left = NULL;
-	n3->right = NULL;
-
-	insert(&processTree, n3);
-
-	printf("%d %d %d\n", processTree->executionTime, processTree->left->executionTime, processTree->right->executionTime);
-
-	process* n4 = (process *)malloc(sizeof(process));
-	n4->processId = 1;
-	n4->executionTime = 7;
-	n4->color = 1;
-	n4->parent = NULL;
-	n4->left = NULL;
-	n4->right = NULL;
-
-	insert(&processTree, n4);
-
-	printf("%d %d %d %d\n", processTree->executionTime, processTree->left->executionTime, processTree->right->executionTime, processTree->left->left->executionTime);
-
 	
-	delete(&processTree, n3);
-
-	printf("%d %d %d\n", processTree->color, processTree->left->color, processTree->right->color);		
-
-	process* search = lookup(processTree, 9);
-
-	printf("%d\n", search->executionTime);*/
 }
 
 void insert5(process** root, process* n)
@@ -511,14 +439,11 @@ void insert5(process** root, process* n)
 
 	g->color = 1; //paint red
 	n->parent->color = 0; //paint black
+	
 	if(n == n->parent->left)
 		right_rotate(root, g);
 	else
-	{
-		//printf("CASE 5\n");
-		left_rotate(root, g);
-	}
-		
+		left_rotate(root, g);		
 }
 
 void insert4(process** root, process* n)
