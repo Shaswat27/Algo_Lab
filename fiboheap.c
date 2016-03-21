ea54407e81f157cdb419da401dee93b23a63b978
Shaswat27 <shaswatdatta@hotmail.com>
2016-03-21 20:17:01 +0530

    Fibonacci Heaps part is done

--- fiboheap.c
+++ fiboheap.c
@@ -109,9 +109,9 @@ void consolidate(node** min, int *size)
 {
 	//create auxillary array A
 	int D = (int)( ceil(log2(*size)) );
-	node** A = (node **)malloc(D*sizeof(node *)); //an array of node pointers
+	node** A = (node **)malloc(10*sizeof(node *)); //an array of node pointers
 	int i = 0,count = 0,d = 0;
-	for(; i<D; i++)
+	for(; i<10; i++)
 		A[i] = NULL;
 
 	node* w = *min;
@@ -149,7 +149,7 @@ void consolidate(node** min, int *size)
 
 	*min = NULL;
 
-	for(i=0; i<D; i++)
+	for(i=0; i<10; i++)
 	{
 		if(A[i] != NULL)
 		{
@@ -173,6 +173,7 @@ void consolidate(node** min, int *size)
 			}
 		}
 	}
+	printf("Here, min = %d\n", (*min)->key);
 	return;
 }
 
@@ -216,6 +217,8 @@ node* extractMin(node** min, int *size)
 			*min = z->right;
 			consolidate(min, size);
 		}	
+
+		//printf("HERE, min = %d\n", (*min)->key);
 
 		*size -= 1;
 	}
@@ -267,13 +270,89 @@ node* fibUnion(node** min1, node** min2, int *size1, int *size2, int *size)
 
 void cut(node** min, node* x, node* y)
 {
+	//remove x from the child list of y, decrement degree[y]
+	if((y)->child->right == (y)->child) //y has only one child
+	{
+		(x)->parent = NULL;
+		
+		(y)->child = NULL;
 
+		//printf("x = %d\n", (x)->key);
+	}
+	else if((y)->child == x)
+	{
+		(x)->right->left = (x)->left;
+		(x)->left->right = (x)->right;
+
+		(y)->child = (x)->right;
+		(x)->parent = NULL;
+	}
+	else
+	{
+		(x)->right->left = (x)->left;
+		(x)->left->right = (x)->right;
+
+		(x)->parent = NULL;
+	}
+
+	//add x to the root list of H
+	(*min)->right->left = (x);
+
+	//printf("HERE, x = %d\n", (x)->key);
+
+	(x)->right = (*min)->right;
+
+
+
+	(*min)->right =(x);
+	(x)->left = (*min);
+
+	//p[x] = NIL
+	(x)->parent = NULL;
+	(x)->mark = 0;
 }
 
+void cascadingCut(node** min, node* y)
+{
+	node* z = (y)->parent;
+
+	if(z != NULL)
+	{
+		if((y)->mark == 0)
+			(y)->mark = 1;
+		else
+		{
+			cut(min, y, z);
+			cascadingCut(min, z);
+		}
+	}
+}
+
+void decreaseKey(node** min, node* x, int k)
+{
+	if(k > (x)->key)
+	{
+		printf("ERROR: new key is greater than previous key!\n");
+		return;
+	}
+
+	(x)->key = k;
+
+	node* y= (x)->parent;
+
+	if(y != NULL && (x)->key < y->key)
+	{
+		cut(min, x, y);
+		cascadingCut(min, y);
+	}
+
+	if((x)->key < (*min)->key)
+		*min = x;
+}
 
 int main()
 {
-	makeFibHeap(&min, &size);
+	/*makeFibHeap(&min, &size);
 
 	int i = 100;
 	for(; i>=1; i--)
@@ -292,9 +371,9 @@ int main()
 		printf("Min: %d\n", y->key);
 	}
 
-	printf("\n");
+	printf("\n");*/
 
-	/*
+	
 	//FIRST HEAP
 	makeFibHeap(&min, &size);
 
@@ -342,6 +421,15 @@ int main()
 	min3 = fibUnion(&min, &min2, &size, &size2, &size3);
 
 	printf("min: %d, min->right: %d, min->right->right: %d, min->child: %d, min->child->left: %d, min->child->right: %d\n", min3->key, min3->right->key, min3->right->right->key, min3->child->key, min3->child->left->key, min3->child->right->key);
-	*/
+	
+	//decrese key
+	decreaseKey(&min3, min3->child, 0);
+
+	printf("min: %d, min->right: %d, min->right->right: %d, min->right->right->right: %d\n", min3->key, min3->right->key, min3->right->right->key, min3->right->right->right->key);
+
+	node* t = extractMin(&min3, &size3);
+
+	printf("min: %d, min->child: %d, min->child->child: %d\n", min3->key, min3->child->key, min3->child->child->key);
+
 	return 0;
 }
